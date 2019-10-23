a1d749bdc340e0bb0ec699f6fc98752072e1d1e0
seelai.wong <seelai.wong@y3technologies>
2019-10-23 10:08:30 +0800

    init

---
+++ ToDoList_Workshop/src/todolist/Task.java
@@ -0,0 +1,32 @@
+package todolist;
+
+public class Task {
+	private String description = null;
+	private boolean isComplete = false;
+	
+	
+	public Task(String description) {
+		super();
+		this.description = description;
+	}
+	
+	public Task(String description, boolean isComplete) {
+		super();
+		this.description = description;
+		this.isComplete = isComplete;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isComplete() {
+		return isComplete;
+	}
+	public void setComplete(boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+
+}
